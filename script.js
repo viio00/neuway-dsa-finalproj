28cf07b06198f31613493d95c5956b8cb0c777fe
alexiscastro-tech <alexis.castro@neu.edu.ph>
2025-11-27 02:01:18 +0800

    idk

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const selector = document.getElementById("Location-select");
+/*const selector = document.getElementById("Location-select");
 const displayImageimage = document.getElementById("displayImage");
 selector.addEventListener("change", () => {
     const value = selector.value;
@@ -8,7 +8,7 @@ selector.addEventListener("change", () => {
         return;
     }
 
-    if (value == "chynnaCute"){
+    if (value == "1"){
         displayImage.src = "chynnaCute.jpg";
     } else if (value == "chynnaAdorable"){
         displayImage.src = "chynnaAdorable.jpg";
@@ -20,5 +20,70 @@ selector.addEventListener("change", () => {
         displayImage.src = "chynnaMatalino.jpg";
     } 
 
-    displayImage.style.display = "block" 
+    displayImage.style.display = "block"
+
+});*/
+
+const suggestionsList = [
+    "Chynna Maganda",
+    "Chynna Smart",
+    "Chynna Pretty",
+    "Chynna Adorable",
+    "Chynna Magaling",
+    "Chynna Cute"
+];
+
+const input = document.getElementById("searchLocation")
+const suggestionsBox = document.getElementById("suggestions")
+
+input.addEventListener("input", () => {
+    const value = input.value.toLowerCase();
+    suggestionsBox.innerHTML = "";
+    
+    if (!value){
+        suggestionsBox.style.display = "none";
+        return;
+    }
+
+    const filtered = suggestionsList.filter(item =>
+        item.toLowerCase().includes(value)
+    );
+
+    if (filtered.length == 0){
+        suggestionsBox.style.display = "none";
+        return;
+    }
+    filtered.forEach(item => {
+        const li = document.createElement("li")
+        li.textContent = item;
+        li.onclick = () => {
+            input.value = item;
+            suggestionsBox.style.display = "none";
+        };
+        suggestionsBox.appendChild(li);
+    });
+    suggestionsBox.style.display = "block";
 });
+
+input.addEventListener("focus", () => {
+    if (suggestionsList.length > 0) {
+        showAllSuggestions();
+    }
+});
+
+function showAllSuggestions() {
+    suggestionsBox.innerHTML = "";
+
+    suggestionsList.forEach(item => {
+        const li = document.createElement("li");
+        li.textContent = item;
+
+        li.onclick = () => {
+            input.value = item;
+            suggestionsBox.display.style = "none";
+        };
+        suggestionsBox.appendChild(li);
+    });
+    suggestionsBox.style.display = "block";
+}
+    
