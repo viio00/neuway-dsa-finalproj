781bef57792a920ac2490d31af20151671ed976a
alexiscastro-tech <alexis.castro@neu.edu.ph>
2025-11-30 23:36:17 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -91,28 +91,29 @@ const searchDestination = document.getElementById("searchDestination");
 const routeImages = document.getElementById("displayImage");
 
 const displayImageList = {
-    "A. Gate-B. Main Blg": "A-B.jpg",
-    "A. Gate-C. Library": "A-C.jpg",
-    "A. Gate-D. IS Blg": "A-D.jpg",
-    "A. Gate-E. PSB": "A-E.jpg",
-    "A. Gate-F. Secret Garden": "A-F.jpg",
-    "A. Gate-G. SOM": "A-G.jpg",
+    "A. Gate-B. Main Blg": "A-B.jpg", "B. Main Blg-A. Gate": "A-B.jpg", 
+    "A. Gate-C. Library": "A-C.jpg", "C. Library-A. Gate": "A-C.jpg",
+    "A. Gate-D. IS Blg": "A-D.jpg", "D. IS Blg-A. Gate": "A-D.jpg",
+    "A. Gate-E. PSB": "A-E.jpg", "E. PSB-A. Gate": "A-E.jpg",
+    "A. Gate-F. Secret Garden": "A-F.jpg", "F. Secret Garden-A. Gate": "A-F.jpg",
+    "A. Gate-G. SOM": "A-G.jpg", "G. SOM-A. Gate": "A-G.jpg",
 
-    "B. Main Blg-C. Library": "B-C.jpg",
-    "B. Main Blg-D. IS Blg": "B-D.jpg",
-    "B. Main Blg-E. PSB": "B-E.jpg",
-    "B. Main Blg-F. Secret Garden": "B-F.jpg",
-    "B. Main Blg-G. SOM": "B-G.jpg",
+    "B. Main Blg-C. Library": "B-C.jpg", "B. Main Blg-C. Library": "B-C.jpg",
+    "B. Main Blg-D. IS Blg": "B-D.jpg", "D. IS Blg-B. Main Blg": "B-D.jpg",
+    "B. Main Blg-E. PSB": "B-E.jpg", "E. PSB-B. Main Blg": "B-E.jpg",
+    "B. Main Blg-F. Secret Garden": "B-F.jpg", "F. Secret Garden-B. Main Blg": "B-F.jpg",
+    "B. Main Blg-G. SOM": "B-G.jpg", "B. Main Blg-G. SOM": "B-G.jpg",
 
-    "C. Library-D. IS Blg": "C-D.jpg",
-    "C. Library-E. PSB": "C-E.jpg",
-    "C. Library-F. Secret Garden": "C-F.jpg",
-    "C. Library-G. SOM": "C-G.jpg",
+    "C. Library-D. IS Blg": "C-D.jpg", "D. IS Blg-C. Library": "C-D.jpg",
+    "C. Library-E. PSB": "C-E.jpg", "E. PSB-C. Library": "C-E.jpg",
+    "C. Library-F. Secret Garden": "C-F.jpg", "F. Secret Garden-C. Library": "C-F.jpg",
+    "C. Library-G. SOM": "C-G.jpg", "G. SOM-C. Library": "C-G.jpg",
     
-    "D. IS Blg-E. PSB": "D-E.jpg",
-    "D. IS Blg-F. Secret Garden": "D-F.jpg",
-    "D. IS Blg-G. SOM": "D-G.jpg"
+    "D. IS Blg-E. PSB": "D-E.jpg", "E. PSB-D. IS Blg": "D-E.jpg",
+    "D. IS Blg-F. Secret Garden": "D-F.jpg", "F. Secret Garden-D. IS Blg": "D-F.jpg",
+    "D. IS Blg-G. SOM": "D-G.jpg", "G. SOM-D. IS Blg": "D-G.jpg"
 
+    
     //"A. Gate"
     //"B. Main Blg"
     //"C. Library"
